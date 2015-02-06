058fa65f8ad9268a147a5582e70c7cf3da80212b
Nick Desaulniers <ndesaulniers@mozilla.com>
2015-02-06 13:39:28 -0800

    use atExit to detect memory leaks

--- src/epilogue.js
+++ src/epilogue.js
@@ -1,11 +1,6 @@
-var lib = {
-  // Seemingly can't use bind here, possible emscripten bug?
-  emscripten_trace_record_allocation: function (address, size) {
-    memProf.mallocProxy(address, size);
-  },
-  emscripten_trace_record_free: function (address) {
-    memProf.freeProxy(address);
-  },
-};
+Module.addOnExit(function () {
+  console.log('runtime exit');
+  console.log(memProf.outstandingMallocs,
+              Object.keys(memProf.outstandingMallocs).length);
+});
 
-mergeInto(LibraryManager.library, lib);
